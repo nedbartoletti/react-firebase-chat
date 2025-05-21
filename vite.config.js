d3aedd9b2ca64acb41edfee96d07d1aaf96e3bb3
Ned Bartoletti <harshil10605@gmail.com>
2025-05-21 11:25:33 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'rollup-plugin-polyfill-node'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        process: true,
+      },
+    })
+  ],
+  define: {
+    'global': 'window',
+  }
 })
