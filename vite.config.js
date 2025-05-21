0a68850ceb4e8616c9e242546221c257a86ae2d1
Ned Bartoletti <harshil10605@gmail.com>
2025-05-21 11:26:19 +0530

    Fix nodePolyfills import and usage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pkg from 'rollup-plugin-polyfill-node'
-const { nodePolyfills } = pkg
+import { nodePolyfills } from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
   plugins: [
